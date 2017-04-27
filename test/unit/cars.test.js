ba28cfe64a42be406711ba0c4ab5e75d20323e3c
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-27 16:17:01 -0700

    First schema validation test passing

--- test/unit/cars.test.js
+++ test/unit/cars.test.js
@@ -1,7 +1,19 @@
 const assert = require('chai').assert;
+const Car = require('../../lib/models/car');
 
 describe('cars model', () => {
-  it('works', () => {
-    assert.ok(true);
+
+  it('validates make and model are required', () => {
+    const car = new Car();
+    
+    return car.validate()
+      .then(() => { throw new Error('Expected validation to fail'); },
+      err => {
+        const errors = err.errors;
+
+        assert.ok(errors.make && errors.make.kind === 'required');
+        assert.ok(errors.model && errors.model.kind === 'required');
+      });
   });
+
 });
