eb3ae20cf1b76cd0f041fd0145249a089e758f1a
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-27 17:17:57 -0700

    added handlers for GET /, GET /:id, POST, and DELETE. Roundtrip test passing

--- test/e2e/cars.test.js
+++ test/e2e/cars.test.js
@@ -11,4 +11,27 @@ describe('cars API', () => {
       .then(req => req.body)
       .then(cars => assert.deepEqual(cars, []));
   });
+
+  let mustang = { make: 'Ford', model: 'Mustang' };
+
+  it('roundtrips a new car', () => {
+    return request
+      .post('/api/cars')
+      .send(mustang)
+      .then(res => res.body)
+      .then(saved => {
+        assert.ok(saved._id, 'saved car has an id');
+
+        mustang = saved;
+      })
+
+      .then(() => {
+        return request.get(`/api/cars/${mustang._id}`);
+      })
+      .then(res => res.body)
+      .then(got => {
+        assert.deepEqual(got, mustang);
+      });
+  });
+
 });
