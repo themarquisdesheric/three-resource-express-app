f803bf58d6343824584e2300639c689a764bab51
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-28 20:25:38 -0700

    First schema test passing

---
+++ test/unit/plants.test.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const Plant = require('../../lib/models/plant');
+
+describe('plants model', () => {
+
+  it('ensures invalid documents are not accepted', () => {
+    const invalidPlant = new Plant();
+
+    return invalidPlant.validate()
+      .then(
+        () => { throw new Error('Expected validation to fail'); },
+        err => {
+          const errors = err.errors;
+          assert.ok(errors.type && errors.leafy.kind === 'required');
+          assert.ok(errors.leafy && errors.leafy.kind === 'required');
+        }
+      );
+  });
+
+});
